6be1a6d00ff163185137d12adc6796addca18e9f
FrankHan1104 <155383217+FrankHan1104@users.noreply.github.com>
2024-01-02 14:33:29 +0900

    Update lezhin.js

--- hokees01/js/lezhin.js
+++ hokees01/js/lezhin.js
@@ -247,7 +247,7 @@ if(!window['lezhin']) {
 
 
 		
-		document.getElementById('roundTitle').innerText = "작동중1...";
+		document.getElementById('statusText').innerText = "작동중1...";
 					let winnerName = _O.Vars.gameHistory["1"][0].name; // 여기서는 게임 히스토리의 첫 번째 요소가 우승자라고 가정
 					
   const db = firebase.database();
