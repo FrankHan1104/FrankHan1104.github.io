8b63c718a032eddf24561dab55216aeb58c95333
FrankHan1104 <155383217+FrankHan1104@users.noreply.github.com>
2024-01-02 13:53:18 +0900

    Update lezhin.js

--- hokees01/js/lezhin.js
+++ hokees01/js/lezhin.js
@@ -235,7 +235,7 @@ if(!window['lezhin']) {
       },
       setRoundTitle() {
         if(_O.Vars.curRound > 1) document.getElementById('roundTitle').innerText = `${_O.Vars.curRound}강 선택`;
-        else document.getElementById('roundTitle').innerText = `축하합니다. 최종 이상형이 선정되었습니다.`;
+       // else document.getElementById('roundTitle').innerText = `축하합니다. 최종 이상형이 선정되었습니다.`;
 	     
       },
       setItem() {
@@ -244,7 +244,7 @@ if(!window['lezhin']) {
         if(!tObj) return;
         tObj.innerHTML = s;
         if(_O.Vars.curRound === 1) { 
-		document.getElementById('statusText').innerText = "작동중...";
+		document.getElementById('roundTitle').innerText = "작동중...";
 					let winnerName = _O.Vars.gameHistory["1"][0].name; // 여기서는 게임 히스토리의 첫 번째 요소가 우승자라고 가정
 					_O.updateWinner(winnerName); // 우승자 업데이트 함수 호출
 		_O.Html.setHistory();
