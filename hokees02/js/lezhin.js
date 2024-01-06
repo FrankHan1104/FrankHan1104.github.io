cee7d0958301d9d114d3807b51c1dfc4a7db56f2
FrankHan1104 <155383217+FrankHan1104@users.noreply.github.com>
2024-01-06 21:57:05 +0900

    Update lezhin.js

--- hokees02/js/lezhin.js
+++ hokees02/js/lezhin.js
@@ -271,7 +271,7 @@ _O.start = () => {
           s += `
           <li>
             <a class="item ${_O.Vars.curRound === 1 ? 'final' : ''}" id="item_${i}" hover="false" href="javascript:void(0);" onclick="lezhin.Event.clickItem(this);" onmouseover="lezhin.Event.overItem(this);" onmouseout="lezhin.Event.outItem(this);">
-              <span class="thumb"><img src="${_O.Vars.gameHistory[_O.Vars.curRound.toString()][i]['imgSrc']}" alt="여자 연예인 사진"></span>
+              <span class="thumb"><img src="${_O.Vars.gameHistory[_O.Vars.curRound.toString()][i]['imgSrc']}" alt="사진"></span>
               <strong> ${_O.Vars.gameHistory[_O.Vars.curRound.toString()][i]['name']}</strong>
             </a>
           </li>
